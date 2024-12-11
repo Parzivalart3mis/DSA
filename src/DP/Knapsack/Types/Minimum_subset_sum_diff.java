1f991f4b819ad2b453dce1c200b2959c1c988c02
Parzivalart3mis <yashvardhansharma.6@gmail.com>
2024-12-11 11:52:08 -0600

    Adding comments

--- src/DP/Knapsack/Types/Minimum_subset_sum_diff.java
+++ src/DP/Knapsack/Types/Minimum_subset_sum_diff.java
@@ -3,48 +3,59 @@ package DP.Knapsack.Types;
 import java.util.ArrayList;
 
 public class Minimum_subset_sum_diff {
-    boolean[][] t;
+    boolean[][] t; // DP table to track subset sums
+
     public int minimumDifference(int[] nums) {
-        int n = nums.length;
-        int range = 0;
-        int mn = Integer.MAX_VALUE;
-        ArrayList<Integer> arr = new ArrayList<Integer>();
+        int n = nums.length; // Number of elements in the array
+        int range = 0; // Total sum of all elements
+        int mn = Integer.MAX_VALUE; // Variable to store the minimum difference
+        ArrayList<Integer> arr = new ArrayList<>(); // List to store possible subset sums
 
-        for(int i=0;i<n;i++) {
-            range = range + nums[i];
+        // Calculate the total sum of the array elements
+        for (int i = 0; i < n; i++) {
+            range += nums[i];
         }
 
-        t = new boolean[n+1][range+1];
+        // Initialize the DP table to track subset sums
+        t = new boolean[n + 1][range + 1];
 
-        for (int i=0; i<n+1;i++) {
-            for(int j=0; j<range+1; j++) {
-                if(i==0)
-                    t[i][j] = false;
-                if(j==0)
-                    t[i][j] = true;
-            }
+        // Base case: A sum of 0 is always possible (empty subset)
+        for (int i = 0; i < n + 1; i++) {
+            t[i][0] = true;
         }
 
-        for(int i =1;i<n+1;i++) {
-            for(int j=1;j<range+1;j++) {
-                if(nums[i-1]<=j) {
-                    t[i][j] = t[i-1][j-nums[i-1]] || t[i-1][j];
+        // Base case: A non-zero sum is not possible with zero elements
+        for (int j = 0; j < range + 1; j++) {
+            t[0][j] = false;
+        }
+
+        // Populate the DP table using the subset sum logic
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < range + 1; j++) {
+                if (nums[i - 1] <= j) {
+                    // Include or exclude the current element
+                    t[i][j] = t[i - 1][j - nums[i - 1]] || t[i - 1][j];
+                } else {
+                    // Exclude the current element
+                    t[i][j] = t[i - 1][j];
                 }
-                else {
-                    t[i][j] = t[i-1][j];
-                }
             }
         }
 
-        for(int j=1;j<(range+1)/2;j++) {
-            if(t[n][j] == true) {
-                arr.add(j);
+        // Find all subset sums up to half the total sum
+        for (int j = 1; j <= range / 2; j++) {
+            if (t[n][j]) { // Check if subset with sum 'j' is possible
+                arr.add(j); // Add it to the list of possible subset sums
             }
         }
 
-        for(int i=0;i<arr.size();i++) {
-            mn = Math.min(mn, range-(2* arr.get(i)));
+        // Calculate the minimum difference between two subsets
+        for (int i = 0; i < arr.size(); i++) {
+            // Minimum difference is |range - 2 * subsetSum|
+            mn = Math.min(mn, range - (2 * arr.get(i)));
         }
+
+        // Return the minimum difference
         return mn;
     }
 }
