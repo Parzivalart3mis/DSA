0aef0fcb5affedab00b35d03ce431dbb3a10eab0
Parzivalart3mis <yashvardhansharma.6@gmail.com>
2024-12-19 11:32:16 -0600

    Binary Search - Search in Rotated Sorted Array

--- src/DSA/Search/Binary_search_SearchInRotatedSortedArray.java
+++ src/DSA/Search/Binary_search_SearchInRotatedSortedArray.java
@@ -1,20 +1,31 @@
 package DSA.Search;
 
-public class Binary_search_Search_in_Rotated_Sorted_Array {
+public class Binary_search_SearchInRotatedSortedArray {
     public int search_pivot(int[] nums) {
         int start = 0;
         int end = nums.length - 1;
 
         while (start <= end) {
+            // Calculate the middle index to divide the search range
             int mid = start + (end - start) / 2;
+
+            // Check if the current middle element is the pivot (greater than its next element)
             if (mid < end && nums[mid] > nums[mid + 1]) {
-                return mid;
-            } else if (mid > start && nums[mid] < nums[mid - 1]) {
-                return mid - 1;
-            } else if (nums[start] >= nums[mid]) {
-                end = mid - 1;
-            } else {
-                start = mid + 1;
+                return mid; // Pivot found at `mid`
+            }
+            // Check if the current middle element is less than its previous element
+            // This means the pivot is `mid - 1`
+            else if (mid > start && nums[mid] < nums[mid - 1]) {
+                return mid - 1; // Pivot found at `mid - 1`
+            }
+            // If the middle element is smaller than or equal to the start element,
+            // the pivot lies in the left half of the array
+            else if (nums[start] >= nums[mid]) {
+                end = mid - 1; // Move the search range to the left half
+            }
+            // Otherwise, the pivot lies in the right half of the array
+            else {
+                start = mid + 1; // Move the search range to the right half
             }
         }
         return -1;
