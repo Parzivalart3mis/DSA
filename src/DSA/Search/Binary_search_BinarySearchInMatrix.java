fce7d9f457d834ddf920480cdfc34219fd2c44ff
Parzivalart3mis <yashvardhansharma.6@gmail.com>
2024-12-23 17:57:15 -0600

    Binary Search - Binary Search in 2-D Matrix(Adding comments)

--- src/DSA/Search/Binary_search_BinarySearchInMatrix.java
+++ src/DSA/Search/Binary_search_BinarySearchInMatrix.java
@@ -6,9 +6,11 @@ public class Binary_search_BinarySearchInMatrix {
         int col = matrix[0].length - 1;
 
         while (row < matrix.length && col >= 0) {
+            // Case 1: target is at element, answer found
             if (matrix[row][col] == target) {
                 return new int[]{row, col};
             }
+            // Case 2: If the element is smaller than target, it means
             if (matrix[row][col] < target) {
                 row++;
             }
