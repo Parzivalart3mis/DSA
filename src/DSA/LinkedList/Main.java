c26b5ed73bc2bd44569f9bca5d01abd4cf7c7ba6
Parzivalart3mis <yashvardhansharma.6@gmail.com>
2025-08-08 15:54:30 -0500

    DSA

---
+++ src/DSA/LinkedList/Main.java
@@ -0,0 +1,13 @@
+package DSA.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        LL list = new LL();
+        list.insertAtFirst(3);
+        list.insertAtFirst(2);
+        list.insertAtFirst(8);
+        list.insertAtFirst(17);
+
+        list.display();
+    }
+}
