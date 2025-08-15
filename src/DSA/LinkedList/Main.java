d5794bb113a5e088e67cfc8abe3bbe7f780e37ce
Parzivalart3mis <yashvardhansharma.6@gmail.com>
2025-08-15 18:00:31 -0500

    DSA

--- src/DSA/LinkedList/Main.java
+++ src/DSA/LinkedList/Main.java
@@ -16,5 +16,22 @@ public class Main {
         list.display();
         System.out.println(list.delete(2));
         list.display();
+
+        System.out.println("********************************************************");
+
+        DLL list2 = new DLL();
+        list2.insertFirst(3);
+        list2.insertFirst(2);
+        list2.insertFirst(8);
+        list2.insertFirst(17);
+        list2.insertLast(99);
+//        list2.insert(100, 3);
+        list2.display();
+//        System.out.println(list2.deleteFirst());
+//        list2.display();
+//        System.out.println(list2.deleteLast());
+//        list2.display();
+//        System.out.println(list2.delete(2));
+//        list2.display();
     }
 }
