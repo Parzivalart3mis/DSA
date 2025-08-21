3661076e9e5e0577bdd1c46a639650922112e333
Parzivalart3mis <yashvardhansharma.6@gmail.com>
2025-08-21 06:56:32 -0500

    DSA

--- src/DSA/LinkedList/LL.java
+++ src/DSA/LinkedList/LL.java
@@ -261,4 +261,28 @@ public class LL {
         }
         return s;
     }
+
+    public boolean isHappy(int n) {
+        int slow = n;
+        int fast = n;
+
+        do {
+            slow = findsquare(slow);
+            fast = findsquare(findsquare(fast));
+        } while (slow != fast);
+
+        if (slow == 1) {
+            return true;
+        }
+        return false;
+    }
+    private int findsquare(int number) {
+        int ans = 0;
+        while (number > 0) {
+            int rem = number % 10;
+            ans = ans + (rem * rem);
+            number /= 10;
+        }
+        return ans;
+    }
 }
