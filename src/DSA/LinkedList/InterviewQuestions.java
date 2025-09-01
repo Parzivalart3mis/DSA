b79115136d0e532a32dedc48180665261207c210
Parzivalart3mis <yashvardhansharma.6@gmail.com>
2025-09-01 18:08:00 -0500

    DSA

--- src/DSA/LinkedList/InterviewQuestions.java
+++ src/DSA/LinkedList/InterviewQuestions.java
@@ -219,7 +219,28 @@ public class InterviewQuestions {
         }
 
         public void reorderList(ListNode head) {
+            if (head == null || head.next == null) {
+                return;
+            }
+            ListNode mid = middleNode(head);
+            ListNode hs = reverseList(mid);
+            ListNode hf = head;
 
+            // Rearrange
+            while (hf != null && hs != null) {
+                ListNode temp = hf.next;
+                hf.next = hs;
+                hf = temp;
+
+                temp = hs.next;
+                hs.next = hf;
+                hs = temp;
+            }
+
+            // Setting next of tail to null
+            if (hf != null) {
+                hf.next = null;
+            }
         }
     }
 }
